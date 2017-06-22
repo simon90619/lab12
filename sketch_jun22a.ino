a7174c40a04277d22b42011ff2ea9bf25171fd61
simon90619 <simon1996524@gmail.com>
2017-06-22 23:29:20 +0800

    Add files via upload

---
+++ sketch_jun22a.ino
@@ -0,0 +1,20 @@
+#include <ESP8266.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(3,2); /* RX:D3, TX:D2 */
+void setup() {
+  mySerial.begin(115200); 
+  Serial.begin(9600);
+  Serial.println("start");
+}
+ 
+void loop() {
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+ 
+  if (Serial.available()) {
+    char chars = Serial.read();
+    mySerial.write(chars);
+  }
+}
